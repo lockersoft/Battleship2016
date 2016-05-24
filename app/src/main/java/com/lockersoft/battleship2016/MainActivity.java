76a79f5e1813c11ec4dbc1201e89ccd3862608a4
Dave Jones <Dave.Jones@scc.spokane.edu>
2016-05-24 09:29:53 -0700

    added list adapter code for displaying all users from the API

--- app/src/main/java/com/lockersoft/battleship2016/MainActivity.java
+++ app/src/main/java/com/lockersoft/battleship2016/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.android.volley.*;
 import com.android.volley.toolbox.ImageLoader;
@@ -38,12 +42,14 @@ public class MainActivity extends BaseActivity implements ServerRequests {
   static Button userPreferences;
   EditText edtUsername, edtPassword, edtGameID;
   NetworkImageView imgAvatar;
+  ListView listView;
 
   @Override
   protected void onCreate( Bundle savedInstanceState ) {
     super.onCreate( savedInstanceState );
     setContentView( R.layout.activity_main );
 
+    listView = (ListView) findViewById( R.id.listView );
     imgAvatar = (NetworkImageView) findViewById( R.id.imgAvatar );
     loginButton = (Button) findViewById( R.id.btnLogin );
     btnStartGame = (Button) findViewById( R.id.btnStartGame );
@@ -62,6 +68,50 @@ public class MainActivity extends BaseActivity implements ServerRequests {
     RequestQueue queue = Volley.newRequestQueue( this );
     sr = new ServerRequest( this, "LOGIN", username, password, loginUrl, queue );
 
+    // Defined Array values to show in ListView
+    String[] values = new String[] {
+            "Dave",
+            "Joe",
+            "Sally",
+            "Sharon",
+            "Karen",
+            "Tom",
+            "Lynn",
+            "Scott"
+    };
+
+    // http://androidexample.com/Create_A_Simple_Listview_-_Android_Example/index.php?view=article_discription&aid=65&aaid=90
+    // Define a new Adapter
+    // First parameter - Context
+    // Second parameter - Layout for the row
+    // Third parameter - ID of the TextView to which the data is written
+    // Fourth - the Array of data
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+    // Assign adapter to ListView
+    listView.setAdapter(adapter);
+
+    // ListView Item Click Listener
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view,
+                              int position, long id) {
+
+        // ListView Clicked item index
+        int itemPosition     = position;
+
+        // ListView Clicked item value
+        String  itemValue    = (String) listView.getItemAtPosition(position);
+
+        // Show Alert
+        Toast.makeText(getApplicationContext(),
+                "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                .show();
+      }
+
+    });
 
     //  sr.setCommand( "GetUsers" );
     //   sr.setUrl( getAllUsersUrl );
@@ -72,6 +122,11 @@ public class MainActivity extends BaseActivity implements ServerRequests {
     if( loggedIn ) {
       startActivity( new Intent( this, BattlePrefs.class ) );
     }
+  }
+
+  public void getAllUsersOnClick( View v ){
+    sr.setUrl( getAllUsersUrl );
+    sr.makeRequest( "GETALLUSERS" );
   }
 
   public void clickLogin( View v ) {
@@ -125,12 +180,21 @@ public class MainActivity extends BaseActivity implements ServerRequests {
     }
   }
 
+  private void processGetAllUsers( String response ){
+    // Parse it out, get strings, put into an ARRAY, associate with the adapter
+  }
+
   @Override
   public void ProcessResponse( String command, String response ) {
     switch( command ) {
       case "LOGIN":
         Log.i( "BattleShip", "LOGIN --- " + response );
         processLogin( response );
+        break;
+
+      case "GETALLUSERS":
+        processGetAllUsers( response );
+        Log.i( "BattleShip", "GETALLUSERS --- " + response );
         break;
 
       case "GETUSERS":
